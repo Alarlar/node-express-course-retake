4f29e6d47305361ff1bffe0941c4213bac9c282f
Alarlar <52812499+Alarlar@users.noreply.github.com>
2025-12-09 12:39:39 -0800

    implementing APIs that return JSON, using API as interface for http to intercat with data

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,20 @@
-console.log('Express Tutorial')
+const express = require("express");
+const app = express();
+const { products } = require("./data");
+
+app.get("/", (req, res) => {
+  res.send("<h1> Home Page</h1><a href='/api/products'>products</a>"); // This is creating API http interface to intercat with data
+});
+
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+  // res.json({ message: "It worked!" });
+});
+
+app.listen(5003, (req, res) => {
+  console.log("Server is listening on port 5003...");
+});
