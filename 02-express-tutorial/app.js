1e65d08686e5b8fd28f5c7cd5d8f72511d549c7f
Alarlar <52812499+Alarlar@users.noreply.github.com>
2025-12-14 14:15:54 -0800

    create a middleware function called logger, with 3 parametrs req, res, next and  log the method and url properties from the req object

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,57 +1,21 @@
 const express = require("express");
 const app = express();
-const { products } = require("./data");
 
-app.get("/", (req, res) => {
-  res.send("<h1> Home Page</h1><a href='/api/products'>products</a>"); // This is creating API http interface to intercat with data
+// req => middleware => res
+
+const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+};
+
+app.get("/", logger, (req, res) => {
+  res.send("Home");
 });
 
-app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.json(newProducts);
-});
-
-app.get("/api/products/:productID", (req, res) => {
-  // console.log(req);
-  // console.log(req.params);
-  const { productID } = req.params;
-
-  const singleProduct = products.find(
-    (products) => products.id === Number(productID)
-  );
-  if (!singleProduct) {
-    return res.status(404).send("Product does not exist");
-  }
-  // console.log(singleProduct);
-  return res.json(singleProduct);
-});
-
-// Route params
-app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
-  console.log(req.params);
-  res.send("hello");
-});
-
-// Query string params
-app.get("/api/v1/query", (req, res) => {
-  // console.log(req.query);
-  const { search, limit } = req.query;
-  let sortProducts = [...products];
-
-  if (search) {
-    sortProducts = sortProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-
-  if (limit) {
-    sortProducts = sortProducts.slice(0, Number(limit));
-  }
-  res.status(200).json(sortProducts);
-  // res.send("hello");
+app.get("/about", (req, res) => {
+  res.send("About");
 });
 
 app.listen(5003, (req, res) => {
