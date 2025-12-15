5d2cd1397d577f90f86357a9ffdf37119be34d1b
Alarlar <52812499+Alarlar@users.noreply.github.com>
2025-12-15 10:41:57 -0800

    implementing app.post for /api/people so this will add to people array. And adding middleware to parse body in json

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,8 +2,18 @@ const express = require("express");
 const app = express();
 let { people } = require("./data");
 
+// static assest
+app.use(express.static("./methods-public"));
+// parse from data
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  res.send("POST");
 });
 
 app.listen(5003, (req, res) => {
