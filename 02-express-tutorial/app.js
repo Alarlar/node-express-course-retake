580f9f4a8f05b6bffed3e94f6b49030966dac2c4
Alarlar <52812499+Alarlar@users.noreply.github.com>
2025-12-18 10:50:01 -0800

    adding value in form from server and dynamically add to frontend

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,11 +4,23 @@ let { people } = require("./data");
 
 // static assest
 app.use(express.static("./methods-public"));
-// parse from data
-app.use(express.urlencoded({ extended: false }));
+// parse form data
+app.use(express.urlencoded({ extended: false })); // HTML form Нужен если данные приходят из HTML формы, Парсит name=John&age=30, //Middleware → переводит тело запроса в JS-объект
+// parse json
+app.use(express.json());
 
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name value" });
+  }
+  res.status(201).json({ success: true, person: name });
 });
 
 app.post("/login", (req, res) => {
@@ -16,23 +28,9 @@ app.post("/login", (req, res) => {
   if (name) {
     return res.status(200).send(`Welcome ${name}`);
   }
-
   res.status(401).send("Please provide a name");
 });
 
-app.post("/api/people", (req, res) => {
-  const { name } = req.body;
-
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Please provide a name" });
-  }
-
-  people.push({ id: people.length + 1, name });
-
-  res.status(201).json({ success: true, name });
-});
 app.listen(5003, (req, res) => {
   console.log("Server is listening on port 5003...");
 });
