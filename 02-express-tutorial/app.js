12d88a2fd3b6a56b011d30fa423a240e528f8f26
Alarlar <52812499+Alarlar@users.noreply.github.com>
2025-12-21 18:05:04 -0800

    refactoring routes, controllers, so the functions in one folder, the routes in another , they are communicate with each other trough exports/imports and test them with Postamn

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-let { people } = require("./data");
+const people = require("./routes/people");
+const auth = require("./routes/auth");
 
 // static assest
 app.use(express.static("./methods-public"));
@@ -9,37 +10,8 @@ app.use(express.urlencoded({ extended: false })); // HTML form Нужен есл
 // parse json
 app.use(express.json());
 
-app.get("/api/people", (req, res) => {
-  res.status(200).json({ success: true, data: people });
-});
-
-app.post("/api/people", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: "please provide name value" });
-  }
-  res.status(201).json({ success: true, person: name });
-});
-
-app.post("/api/postman/people", (req, res) => {
-  const { name } = req.body;
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: "please provide name value" });
-  }
-  res.status(201).json({ success: true, data: [...people, name] });
-});
-
-app.post("/login", (req, res) => {
-  const { name } = req.body;
-  if (name) {
-    return res.status(200).send(`Welcome ${name}`);
-  }
-  res.status(401).send("Please provide a name");
-});
+app.use("/api/people", people);
+app.use("/login", auth);
 
 app.listen(5003, (req, res) => {
   console.log("Server is listening on port 5003...");
